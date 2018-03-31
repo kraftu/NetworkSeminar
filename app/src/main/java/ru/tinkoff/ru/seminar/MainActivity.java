29d05d4b30177e0d64e22c4eaf04cbe05e070914
Erofeev Evgeniy <e.erofeev2@tinkoff.ru>
2018-03-31 11:58:37 +0300

    init

--- app/src/main/java/ru/tinkoff/ru/seminar/MainActivity.java
+++ app/src/main/java/ru/tinkoff/ru/seminar/MainActivity.java
@@ -1,13 +1,50 @@
 package ru.tinkoff.ru.seminar;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.concurrent.Callable;
+
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultTextView = findViewById(R.id.resultContent);
+        findViewById(R.id.performRequest).setOnClickListener(v -> performRequest());
+    }
+
+    private void printResult(@Nullable String resultString) {
+        resultTextView.setText(resultString);
+    }
+
+    private void performRequest() {
+        Single.fromCallable(new BackgroundTask())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        this::printResult,
+                        throwable -> printResult(
+                                "Throwable:\n" + throwable.toString()
+                        )
+                );
+    }
+
+    static class BackgroundTask implements Callable<String> {
+
+        @Override
+        public String call() throws Exception {
+            return "BackgroundTask";
+        }
     }
 }
+
